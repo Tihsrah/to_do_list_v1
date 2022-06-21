f76ed5042a7a331f1842932515caf7e4ce2241db
Tihsrah <harshlf4@gmail.com>
2022-06-21 17:34:29 +0530

    added ejs codes for templating

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express= require('express');
 const bodyParser=require('body-parser');
 const app=express();
-
+app.set('view engine','ejs');
 app.listen('3000',function(){});
 
 app.get('/',function(req,res){
-  res.sendFile(__dirname+'/index.html');
+  var currentDay= new Date();
+  var day=currentDay.getDay();
+  var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+  res.render('list',{today:daylist[day]});
+
+
 })
