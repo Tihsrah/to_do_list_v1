683f44069933e1961eddb9c4e532128c0e16f04d
Tihsrah <harshlf4@gmail.com>
2022-06-22 14:05:39 +0530

    css ejs appjs all updated

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express= require('express');
 const bodyParser=require('body-parser');
 const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"))
 app.set('view engine','ejs');
+
+
+var items=[];
+
 app.listen('3000',function(){});
 
 app.get('/',function(req,res){
   var currentDay= new Date();
-  var day=currentDay.getDay();
-  var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-  res.render('list',{today:daylist[day]});
+  const options = { weekday: 'long', month: 'long', day: 'numeric' };
+  var day=currentDay.toLocaleDateString("en-US",options);
+
+  res.render('list',{today:day,test:items});
 
 
+})
+
+app.post('/',function(req,res){
+  // var body=__dirname+"/list.ejs";
+  var item=req.body.input;
+  items.push(item);
+  res.redirect('/');
 })
